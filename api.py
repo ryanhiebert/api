1020b2dc31b688434130a1175570b5ad06d82b6e
Ryan Hiebert <ryan@ryanhiebert.com>
2014-03-28 01:51:02 -0500

    Make first draft of home page

--- api.py
+++ api.py
@@ -1,8 +1,46 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+def siren(view):
+    """Decorator to make a jsonify response use siren media type"""
+    def _siren(*args, **kwargs):
+        response = view(*args, **kwargs)
+        response.mimetype = 'application/vnd.siren+json'
+        return response
+    return _siren
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return jsonify({
+        'properties': {
+            'name': 'Ryan Hiebert',
+            'age': 25,
+            'interests': [
+                'repeatable deployments',
+                'hypermedia api design',
+                'personal finance',
+                'doing better than just "Don\'t be evil"',
+                'helping people win with money',
+                'immutability in programming languages',
+                'learning forever more',
+                'being a better human being',
+            ]
+        },
+        'actions': [
+            {
+                'name': 'right-for-you',
+                'title': 'Why I\'m the right person to work for you',
+                'method': 'GET',
+                'href': '/right-for-you',
+                'fields': {
+                    'name': 'company',
+                    'type': 'text',
+                }
+            }
+        ]
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
