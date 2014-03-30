8bf92d0a56a8754ed6e45c7ca61d3a6f54822505
Ryan Hiebert <ryan@ryanhiebert.com>
2014-03-29 23:30:14 -0500

    Reverse misunderstood list comprehension order

--- tictactoe.py
+++ tictactoe.py
@@ -16,7 +16,7 @@ class TicTacToe(tuple):
 
     def __str__(self):
         """Convert to a string, using '-' for an empty space."""
-        indexes = ((r,c) for c in range(3) for r in range(3))
+        indexes = ((r,c) for r in range(3) for c in range(3))
         reprs = ['-' if self[r][c] is None else self[r][c] for r,c in indexes]
         return ''.join(reprs)
 
@@ -54,20 +54,20 @@ class TicTacToe(tuple):
 
     def empty(self):
         """Returns the row, column index of empty spaces"""
-        indexes = [(r,c) for c in range(3) for r in range(3)]
+        indexes = [(r,c) for r in range(3) for c in range(3)]
         return [(r,c) for r,c in indexes if self[r][c] is None]
 
     def full(self):
-        indexes = [(r,c) for c in range(3) for r in range(3)]
+        indexes = [(r,c) for r in range(3) for c in range(3)]
         return all(self[r][c] is not None for r,c in indexes)
 
     def turn(self):
-        flat = [self[r][c] for c in range(3) for r in range(3)]
+        flat = [self[r][c] for r in range(3) for c in range(3)]
         return 'o' if flat.count('o') < flat.count('x') else 'x'
 
     def winner(self):
-        row_vectors = [[(r,c) for c in range(3)] for r in range(3)]
-        col_vectors = [[(r,c) for r in range(3)] for c in range(3)]
+        row_vectors = [[(r,c) for r in range(3)] for c in range(3)]
+        col_vectors = [[(r,c) for c in range(3)] for r in range(3)]
         diagonals = [((0,0), (1,1), (2,2)), ((0,2), (1,1), (2,0))]
         win_vectors = row_vectors + col_vectors + diagonals
 
